ad359de91271b9c7ca4ecf9a9c7e79b25bddaecf
Sirarpi647 <osx@Sirarpis-MacBook-Air.local>
2021-05-14 00:40:21 +0400

    mm

--- public/js/main.js
+++ public/js/main.js
@@ -220,8 +220,8 @@ function loadEditItem() {
 }    
 function loadHobbyEditItem() {
     localStorage = window.localStorage;
-    editItem = JSON.parse(localStorage.getItem("editItem"));
-    console.log(editItem);
+    editItem = JSON.parse(localStorage.getItem("HobbyEditItem"));
+    console.log(HobbyEditItem);
     document.getElementById("_id").innerHTML = editItem["_id"];
     document.getElementById("fname").value = editItem["fname"];
     document.getElementById("question1").value = editItem["question1"];   
